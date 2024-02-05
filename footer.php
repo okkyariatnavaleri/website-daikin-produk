c4a7b0d41a1e33b7740ff77b60bea342c8412748
okkyariatnavaleri <okkyaryadna97@gmail.com>
2024-02-05 23:09:07 +0700

    update link facebook

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <h5 class="text-center mb-4">Temui Kami</h5>
         <div class="d-flex justify-content-center align-items-center gap-3 mb-2"> <!-- Added align-items-center -->
-            <a href="https://web.facebook.com/royallestari.id"><i class="fab fa-facebook fs-2"></i></a>
+            <a href="https://www.facebook.com/royallestari.id"><i class="fab fa-facebook fs-2"></i></a>
             <a href="https://www.instagram.com/daikinishop_pekanbaru/"><i class="fab fa-instagram fs-2"></i></a>
             <a href="https://www.youtube.com/@royallestari"><i class="fab fa-youtube fs-2"></i></a>
             <a href="mailto:mellisalestarisinergi@gmail.com"><i class="far fa-envelope fs-2"></i></a>
